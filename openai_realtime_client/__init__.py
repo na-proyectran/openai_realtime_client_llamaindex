003f900c3bf58216ad55bff665f65f2d00cefbff
na-proyectran <nestor.almeida@proyectran.com>
2025-08-14 13:43:58 +0100

    Add WebRTC server examples and handler

--- openai_realtime_client/__init__.py
+++ openai_realtime_client/__init__.py
@@ -2,6 +2,7 @@ from .client.realtime_client import RealtimeClient, TurnDetectionMode
 from .handlers.audio_handler import AudioHandler
 from .handlers.input_handler import InputHandler
 from .handlers.ws_handler import WsHandler
+from .handlers.rtc_handler import RtcHandler
 
 __all__ = [
     "RealtimeClient",
@@ -9,4 +10,5 @@ __all__ = [
     "AudioHandler",
     "InputHandler",
     "WsHandler",
+    "RtcHandler",
 ]
--- openai_realtime_client/handlers/__init__.py
+++ openai_realtime_client/handlers/__init__.py
@@ -1,9 +1,11 @@
 from .input_handler import InputHandler
 from .audio_handler import AudioHandler
 from .ws_handler import WsHandler
+from .rtc_handler import RtcHandler
 
 __all__ = [
     "AudioHandler",
     "InputHandler",
     "WsHandler",
+    "RtcHandler",
 ]
